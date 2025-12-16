30c6cabd83008ef80db59aa16b13668ad37e13ef
Sharmaji78-create <ashishsharmaw2005@gmail.com>
2025-12-17 01:21:11 +0530

    Add files via upload

---
+++ hanu.py
@@ -0,0 +1,18 @@
+
+# first assignment first task
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a + b
+print("addition is: ",c)
+d = a - b
+print("subtraction is: ",d)
+e = a * b
+print("multiplication is: ",e)
+f = a / b
+print("division is: ",f)              
+# first assignment second task
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+c = a + " " + b 
+#print(c)
+print("Hello,", (c),"! Welcome to the Python program.")
